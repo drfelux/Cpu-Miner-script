ec35fa6fa8381a778cdfba6382c6e98d37bee0e6
felixgouin <felixgouin26@gmail.com>
2020-04-22 12:50:48 +0200

    Add files via upload

---
+++ cpuminer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install cmake build-essential libboost-all-dev
+git clone -b Linux https://github.com/nicehash/nheqminer.git
+cd nheqminer/cpu_xenoncat/Linux/asm/
+sh assemble.sh
+cd ../../../Linux_cmake/nheqminer_cpu
+cmake .
+make -j $(nproc)
+./nheqminer_cpu -b
