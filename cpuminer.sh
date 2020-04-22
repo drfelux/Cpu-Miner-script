dfafc755c15f1bd09dc2a346fcb313901713c2a5
felixgouin <felixgouin26@gmail.com>
2020-04-23 00:45:59 +0200

    Update cpuminer.sh

--- cpuminer.sh
+++ cpuminer.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install cmake build-essential libboost-all-dev
 git clone -b Linux https://github.com/nicehash/nheqminer.git
-cd ../../../nheqminer/cpu_xenoncat/Linux/asm/
+cd /Cpu-Miner-script
+cd /nheqminer/cpu_xenoncat/Linux/asm/
 sh assemble.sh
 cd ../../../Linux_cmake/nheqminer_cpu
 cmake .
