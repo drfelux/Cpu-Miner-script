91606d0150845367542649a895e5d1c3c010b766
felixgouin <felixgouin26@gmail.com>
2020-04-22 13:05:53 +0200

    Fix

--- cpuminer.sh
+++ cpuminer.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install cmake build-essential libboost-all-dev
 git clone -b Linux https://github.com/nicehash/nheqminer.git
-cd nheqminer/cpu_xenoncat/Linux/asm/
+cd ../../../nheqminer/cpu_xenoncat/Linux/asm/
 sh assemble.sh
 cd ../../../Linux_cmake/nheqminer_cpu
 cmake .
